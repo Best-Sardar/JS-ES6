3fc283eea53c5facba305a3237d5b276c1dc6684
Seyyedmahdi Seyyedhosseini <seyyedmahdi.sh@gmail.com>
2018-03-30 16:17:50 +0430

    add code for Numbers Helper Methods & Module in javascript & Class in javascript

--- app/index.js
+++ app/index.js
@@ -54,39 +54,94 @@ Spread_operator(1, 5, 6, 8, 10);
 
 
 // // Destructuring Assignment on Arrays and Objects
-let array4 = [" seyyed " ," mahdi " ," hosseini "];
-let [array4one ,array4two] = array4 ;
-console.log(array4one + array4two , " : Destructuring Assignment");
+let array4 = [" seyyed ", " mahdi ", " hosseini "];
+let [array4one, array4two] = array4;
+console.log(array4one + array4two, " : Destructuring Assignment");
 
-let obj = {name : " mahdi " , family : " hosseini " };
-let {name , family } = obj; // must have name simple the key name of object 
+let obj = {
+    name: " mahdi ",
+    family: " hosseini "
+};
+let {
+    name,
+    family
+} = obj; // must have name simple the key name of object 
 console.log(name + family, " : Destructuring Assignment");
 
-let obj1 = {name1 : " mahdi " , family1 : " hosseini " };
-let name1 , family1;
-({name1 ,family1} = obj1); // if we define variable in previus line we must use () for do this 
+let obj1 = {
+    name1: " mahdi ",
+    family1: " hosseini "
+};
+let name1, family1;
+({
+    name1,
+    family1
+} = obj1); // if we define variable in previus line we must use () for do this 
 console.log(name1 + family1, " : Destructuring Assignment");
 
 // arrow functions .........................................................................................
-let  helloword = () => {return "HELLO WORLD :| ";};
+let helloword = () => {
+    return "HELLO WORLD :| ";
+};
 console.log(helloword(), " : Arrow Function");
 // another example
-setTimeout(() => {console.log("HELLO WORLD :| ", " : Arrow Function");},3000);
+setTimeout(() => {
+    console.log("HELLO WORLD :| ", " : Arrow Function");
+}, 3000);
 
 // map property ............................................................................................
-let array5 = [1 ,2 ,3];
-console.log(array5.map((n) => n+5) , " : Map and Filter");
+let array5 = [1, 2, 3];
+console.log(array5.map((n) => n + 5), " : Map and Filter");
 
-let array6 = [10 ,20 ,30 ,40 ,50 ,60 ,70 ];
+let array6 = [10, 20, 30, 40, 50, 60, 70];
 console.log(array6.filter((n) => n > 40));
 
 // String Helper Methods ...................................................................................
 let string1 = `hell${"oOo".repeat(10)} :))`;
-console.log(string1 ," : String Helper Methods");
+console.log(string1, " : String Helper Methods");
 
 let boolean1 = "seyyedmahdi".startsWith("seyyed");
 console.log(boolean1, " : String Helper Methods"); // returned boolean : true
 console.log("seyyed mahdi".endsWith("mahdi"), " : String Helper Methods");
-console.log("seyyed mahdi".includes("ma") , " : String Helper Methods");
+console.log("seyyed mahdi".includes("ma"), " : String Helper Methods");
 // and more ....
 
+// Numbers Helper Methods
+let number1 = (num) => {
+    console.log(Number.isFinite(num), " : Numbers Helper Methods");
+}
+let number2 = (num) => {
+    console.log(Number.isSafeInteger(num), " : Numbers Helper Methods");
+}
+number1(123456789); //  return true
+number2(Math.pow(25, 50)); // return false
+
+//  Module in javascript
+import need from './moduletest';
+console.log(need.show(), " : Module in javascript");
+
+// Class in javascript
+class animal {
+    constructor(name, height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    helo() {
+        console.log(`hello ${this.name} . are your height is ${this.height} ??`,
+        " : Class in javascript");
+    }
+}
+class Lion extends animal{
+    constructor(name ,height ,color) {
+        super(name ,height);
+        this.color = color;        
+    }
+}
+
+let king = new animal("lion", "1.5");
+console.log(king, " : Class in javascript");
+king.helo();
+
+let lion = new Lion("mos" , 1.8 , "golden");
+console.log(lion, " : Class in javascript");
---
+++ app/moduletest.js
@@ -0,0 +1,7 @@
+const need = {
+    name : "mahdi ",
+    family : "hosseini",
+    show : function(){return this.name + this.family + ":))"}
+}
+
+export default need; 
